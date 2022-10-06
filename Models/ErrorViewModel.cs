f65ac3e24ab8b21f863ee078a7f73d5e353b213b
SamiAyala <samirayala23@gmail.com>
2022-10-06 09:39:40 -0300

    create mvc project and add gitignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP9_Final_Integrador.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
